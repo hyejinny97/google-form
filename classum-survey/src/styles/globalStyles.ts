a93a3d77381f3dd185ac80189f52318eb62180fa
hyejin <yhj970102@gmail.com>
2023-12-05 20:44:32 +0900

    [Feat] globalStyles.ts 생성

---
+++ classum-survey/src/styles/globalStyles.ts
@@ -0,0 +1,23 @@
+import { css } from "@emotion/react";
+
+const globalStyles = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
+
+export { globalStyles };
