9cfd2101f2cfa33a10c7a358e3199c251abfe972
hyejin <yhj970102@gmail.com>
2023-12-07 22:09:12 +0900

    [Design] App 컴포넌트에 padding, bgColor 추가

--- classum-survey/src/App.tsx
+++ classum-survey/src/App.tsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom";
 import styled from "@emotion/styled";
-import { Container } from "@mui/material";
-
-const StyledApp = styled.div`
-  width: 100%;
-  min-height: 100vh;
-`;
+import { Container, useTheme } from "@mui/material";
 
 function App() {
+  const theme = useTheme();
+
+  const StyledApp = styled.div`
+    padding: 3rem 0;
+    width: 100%;
+    min-height: 100vh;
+    background-color: ${theme.palette.grey[100]};
+  `;
+
   return (
     <StyledApp>
       <Container maxWidth="md">
