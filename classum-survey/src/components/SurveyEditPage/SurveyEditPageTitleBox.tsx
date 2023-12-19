2de90df34a5444895555858d4555d4e2b8b5c462
hyejin <yhj970102@gmail.com>
2023-12-19 17:16:49 +0900

    [Feat] SurveyEditPageTitleBox에 updateSurveyTitle 관련 dispatch 추가

--- classum-survey/src/components/SurveyEditPage/SurveyEditPageTitleBox.tsx
+++ classum-survey/src/components/SurveyEditPage/SurveyEditPageTitleBox.tsx
@@ -1,12 +1,25 @@
+import { useSelector, useDispatch, shallowEqual } from "react-redux";
 import TextField from "@mui/material/TextField";
 import { SurveyTitleBox } from "@components";
+import { updateSurveyTitle } from "@stores";
+import type { RootState } from "@stores";
 
 function SurveyEditPageTitleBox() {
+  const [surveyTitle] = useSelector(
+    (state: RootState) => [state.survey.surveyTitle],
+    shallowEqual
+  );
+  const dispatch = useDispatch();
+
+  const handleSurveyTitleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    dispatch(updateSurveyTitle(e.target.value));
+
   return (
     <SurveyTitleBox>
       <TextField
+        value={surveyTitle}
+        onChange={handleSurveyTitleChange}
         variant="standard"
-        defaultValue="제목 없는 설문지"
         fullWidth
         InputProps={{ sx: { fontSize: 30 } }}
       />
