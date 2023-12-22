1cc0f75a55be954179de3cd7b711a4ad261c593a
hyejin <yhj970102@gmail.com>
2023-12-22 16:03:17 +0900

    [Feat] SurveyPreviewPage에 util box 추가

--- classum-survey/src/pages/SurveyPreviewPage.tsx
+++ classum-survey/src/pages/SurveyPreviewPage.tsx
@@ -1,10 +1,44 @@
-import { SurveyPreviewPageTitleBox } from "@components";
+import styled from "@emotion/styled";
+import {
+  SurveyPreviewPageTitleBox,
+  SurveyPreviewPageUtilBox,
+} from "@components";
+
+const StyledSurveyPreviewPage = styled.div`
+  position: relative;
+`;
+
+const Left = styled.div`
+  width: 100%;
+`;
+
+const Right = styled.div`
+  position: absolute;
+  top: 0;
+  left: 100%;
+  margin-left: 1rem;
+  min-height: 100%;
+`;
+
+const StyledSurveyPreviewPageUtilBox = styled(SurveyPreviewPageUtilBox)`
+  position: sticky;
+  top: 1rem;
+
+  @media (max-width: 1050px) {
+    display: none;
+  }
+`;
 
 function SurveyPreviewPage() {
   return (
-    <div>
-      <SurveyPreviewPageTitleBox />
-    </div>
+    <StyledSurveyPreviewPage>
+      <Left>
+        <SurveyPreviewPageTitleBox />
+      </Left>
+      <Right>
+        <StyledSurveyPreviewPageUtilBox />
+      </Right>
+    </StyledSurveyPreviewPage>
   );
 }
 
