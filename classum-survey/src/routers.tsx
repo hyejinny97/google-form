e71795bcd2e215ebe4736261a77e92ffc66fcba6
hyejin <yhj970102@gmail.com>
2023-12-22 14:48:22 +0900

    [Refactor] routers path를 상수 변수로 전환

--- classum-survey/src/routers.tsx
+++ classum-survey/src/routers.tsx
@@ -1,6 +1,11 @@
 import { createBrowserRouter, redirect } from "react-router-dom";
 import App from "./App";
 import { SurveyEditPage, SurveyPreviewPage, SurveySubmitPage } from "@pages";
+import {
+  PATH_SURVEY_EDIT,
+  PATH_SURVEY_PREVIEW,
+  PATH_SURVEY_SUBMIT,
+} from "@constants";
 
 const router = createBrowserRouter([
   {
@@ -12,15 +17,15 @@ const router = createBrowserRouter([
         loader: () => redirect("survey/edit"),
       },
       {
-        path: "survey/edit",
+        path: PATH_SURVEY_EDIT,
         element: <SurveyEditPage />,
       },
       {
-        path: "survey/preview",
+        path: PATH_SURVEY_PREVIEW,
         element: <SurveyPreviewPage />,
       },
       {
-        path: "survey/submit",
+        path: PATH_SURVEY_SUBMIT,
         element: <SurveySubmitPage />,
       },
     ],
