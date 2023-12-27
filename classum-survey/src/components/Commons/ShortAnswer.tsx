c84b373166120cdfd77674f8d0b7640c6feb42d7
hyejin <yhj970102@gmail.com>
2023-12-27 21:42:52 +0900

    [Feat] ShortAnswer 컴포넌트에 value, onChange props 추가

--- classum-survey/src/components/Commons/ShortAnswer.tsx
+++ classum-survey/src/components/Commons/ShortAnswer.tsx
@@ -1,16 +1,22 @@
 import { TextField } from "@mui/material";
 
 interface ShortAnswerProps {
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   disabled?: boolean;
   fullWidth?: boolean;
 }
 
 function ShortAnswer({
+  value,
+  onChange,
   disabled = false,
   fullWidth = false,
 }: ShortAnswerProps) {
   return (
     <TextField
+      value={value}
+      onChange={onChange}
       hiddenLabel
       placeholder="단답형 텍스트"
       variant="standard"
