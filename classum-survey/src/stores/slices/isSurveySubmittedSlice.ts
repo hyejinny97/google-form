f9d7187ce4e0d9f1a1f921de57ea6d439060b7c9
hyejin <yhj970102@gmail.com>
2023-12-28 23:09:29 +0900

    [Fix] isSurveySubmittedSlice의 initialState를 pathname에 따라 결정하도록 수정

--- classum-survey/src/stores/slices/isSurveySubmittedSlice.ts
+++ classum-survey/src/stores/slices/isSurveySubmittedSlice.ts
@@ -1,8 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { PATH_SURVEY_PREVIEW_SUBMIT } from "@constants";
 
 export const isSurveySubmittedSlice = createSlice({
   name: "isSurveySubmitted",
-  initialState: false,
+  initialState: location.pathname === PATH_SURVEY_PREVIEW_SUBMIT,
   reducers: {
     submitSurvey() {
       return true;
