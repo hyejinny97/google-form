e0a59804132a75ff9159525bbccdc91bdd881a7b
hyejin <yhj970102@gmail.com>
2023-12-28 22:26:26 +0900

    [Remove] SurveySubmitPage 삭제

--- classum-survey/src/pages/index.ts
+++ classum-survey/src/pages/index.ts
@@ -1,3 +1,2 @@
 export { default as SurveyEditPage } from "./SurveyEditPage";
 export { default as SurveyPreviewPage } from "./SurveyPreviewPage";
-export { default as SurveySubmitPage } from "./SurveySubmitPage";
