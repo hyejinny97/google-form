2785097eb4280e693b3150968f334e15ade11726
hyejin <yhj970102@gmail.com>
2023-12-28 22:23:28 +0900

    [Feat] SurveyPreviewPageQuestionList에서 isSurveySubmitted state 연결

--- classum-survey/src/components/SurveyPreviewPage/SurveyPreviewPageQuestionList.tsx
+++ classum-survey/src/components/SurveyPreviewPage/SurveyPreviewPageQuestionList.tsx
@@ -4,10 +4,18 @@ import type { RootState } from "@stores";
 
 function SurveyPreviewPageQuestionList() {
   const questions = useSelector((state: RootState) => state.survey.questions);
+  const isSurveySubmitted = useSelector(
+    (state: RootState) => state.isSurveySubmitted
+  );
+
   return (
     <>
       {questions.map((question) => (
-        <SurveyPreviewPageQuestionBox key={question.id} data={question} />
+        <SurveyPreviewPageQuestionBox
+          key={question.id}
+          data={question}
+          disabled={isSurveySubmitted}
+        />
       ))}
     </>
   );
