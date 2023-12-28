bd818151933921860c9c4b6a47db44a2986110ae
hyejin <yhj970102@gmail.com>
2023-12-28 22:22:48 +0900

    [Feat] SurveySubmitButton에 dispatch 추가

--- classum-survey/src/components/SurveyPreviewPage/SurveySubmitButton.tsx
+++ classum-survey/src/components/SurveyPreviewPage/SurveySubmitButton.tsx
@@ -1,8 +1,16 @@
+import { useDispatch } from "react-redux";
 import { Button } from "@mui/material";
+import { submitSurvey } from "@stores";
 
 function SurveySubmitButton() {
+  const dispatch = useDispatch();
+
+  const handleButtonClick = () => {
+    dispatch(submitSurvey());
+  };
+
   return (
-    <Button variant="contained" color="secondary">
+    <Button variant="contained" color="secondary" onClick={handleButtonClick}>
       제출
     </Button>
   );
