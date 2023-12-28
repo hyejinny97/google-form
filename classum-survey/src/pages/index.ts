f95c219d691d608c8e0832d7277e00be49924cb7
hyejin <yhj970102@gmail.com>
2023-12-28 23:02:47 +0900

    [Feat] SurveySubmitFooter 생성

--- classum-survey/src/pages/index.ts
+++ classum-survey/src/pages/index.ts
@@ -1,3 +1,4 @@
 export { default as SurveyEditPage } from "./SurveyEditPage";
 export { default as SurveyPreviewPage } from "./SurveyPreviewPage";
 export { default as SurveyPreviewFooter } from "./SurveyPreviewFooter";
+export { default as SurveySubmitFooter } from "./SurveySubmitFooter";
