e131addb18ea005d74bc04c5fca1671160fc011b
hyejin <yhj970102@gmail.com>
2023-12-30 13:32:06 +0900

    [Feat] SurveyQuestionBox에 className prop 추가

--- classum-survey/src/components/Commons/SurveyQuestionBox.tsx
+++ classum-survey/src/components/Commons/SurveyQuestionBox.tsx
@@ -2,11 +2,16 @@ import { Box } from "@components";
 import { BOX_MARGIN } from "@constants";
 
 interface SurveyQuestionBoxProps {
+  className?: string;
   children?: React.ReactNode | string;
 }
 
-function SurveyQuestionBox({ children }: SurveyQuestionBoxProps) {
-  return <Box sx={{ mt: BOX_MARGIN }}>{children}</Box>;
+function SurveyQuestionBox({ className, children }: SurveyQuestionBoxProps) {
+  return (
+    <Box className={className} sx={{ mt: BOX_MARGIN }}>
+      {children}
+    </Box>
+  );
 }
 
 export default SurveyQuestionBox;
