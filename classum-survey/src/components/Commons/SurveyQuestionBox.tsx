2065d41dccd6001b8abc109bf51dabda6b719fcd
hyejin <yhj970102@gmail.com>
2024-01-06 13:13:01 +0900

    [Refactor] SurveyQuestionBox 컴포넌트를 forwardRef로 감쌈

--- classum-survey/src/components/Commons/SurveyQuestionBox.tsx
+++ classum-survey/src/components/Commons/SurveyQuestionBox.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from "react";
 import { Box } from "@components";
 import { BOX_MARGIN } from "@constants";
 
@@ -6,12 +7,14 @@ interface SurveyQuestionBoxProps {
   children?: React.ReactNode | string;
 }
 
-function SurveyQuestionBox({ className, children }: SurveyQuestionBoxProps) {
-  return (
-    <Box className={className} sx={{ mt: BOX_MARGIN }}>
-      {children}
-    </Box>
-  );
-}
+const SurveyQuestionBox = forwardRef(
+  ({ className, children }: SurveyQuestionBoxProps, ref) => {
+    return (
+      <Box className={className} sx={{ mt: BOX_MARGIN }} ref={ref}>
+        {children}
+      </Box>
+    );
+  }
+);
 
 export default SurveyQuestionBox;
