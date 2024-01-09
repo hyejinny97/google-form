3f7da2676cb7f4f16f196cd53542f34887d4950a
hyejin <yhj970102@gmail.com>
2024-01-09 17:58:17 +0900

    [Fix] SurveyEditPageDropdownAnswer에서 useDragAndDropList의 containerRef -> dragItemsRef prop으로 수정

--- classum-survey/src/components/SurveyEditPage/SurveyEditPageDropdownAnswer.tsx
+++ classum-survey/src/components/SurveyEditPage/SurveyEditPageDropdownAnswer.tsx
@@ -22,7 +22,7 @@ function SurveyEditPageDropdownAnswer({
   value: choiceOptions,
   onChange: setChoiceOptions,
 }: SurveyEditPageDropdownAnswerProps) {
-  const container = useRef<HTMLDivElement>(null);
+  const dragItems = useRef<Array<HTMLDivElement>>([]);
   const { handleOptionChange, handleOptionDelete, handleOptionAdd } = useOption(
     { choiceOptions, setChoiceOptions }
   );
@@ -33,7 +33,7 @@ function SurveyEditPageDropdownAnswer({
     handleDragOver,
     handleDragEnd,
   } = useDragAndDropList({
-    containerRef: container,
+    dragItemsRef: dragItems,
     data: choiceOptions,
     handleDataChange: (newData: Array<DataType>) => {
       setChoiceOptions(newData as Array<OptionType>);
@@ -42,11 +42,12 @@ function SurveyEditPageDropdownAnswer({
 
   return (
     <Stack spacing={1}>
-      <DragContainer ref={container}>
+      <DragContainer>
         {choiceOptions.map((option, idx) => {
           return (
             <DragTarget
               key={option.id}
+              ref={(el: HTMLDivElement) => (dragItems.current[idx] = el)}
               targetId={option.id}
               handleDragStart={handleDragStart}
               handleDrag={handleDrag}
