48a5ec12995230260dc91be9cb36cb7e237f5ee9
hyejin <yhj970102@gmail.com>
2024-01-09 16:20:39 +0900

    [Feat] VerticalDragIndicatorIcon 컴포넌트의 className에 CLASSNAME_VERTICAL_INDICATOR 추가

--- classum-survey/src/components/Icons/VerticalDragIndicatorIcon.tsx
+++ classum-survey/src/components/Icons/VerticalDragIndicatorIcon.tsx
@@ -1,7 +1,18 @@
 import { DragIndicatorIcon } from "@components";
+import { CLASSNAME_VERTICAL_INDICATOR } from "@constants";
 
-function VerticalDragIndicatorIcon() {
-  return <DragIndicatorIcon color="disabled" />;
+interface VerticalDragIndicatorIconProp {
+  className?: string;
+}
+
+function VerticalDragIndicatorIcon({
+  className,
+}: VerticalDragIndicatorIconProp) {
+  return (
+    <span className={`${CLASSNAME_VERTICAL_INDICATOR} ${className}`}>
+      <DragIndicatorIcon color="disabled" />
+    </span>
+  );
 }
 
 export default VerticalDragIndicatorIcon;
