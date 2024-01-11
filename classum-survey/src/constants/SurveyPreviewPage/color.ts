0bd2233d36bddf0d8eb46ba1bedd31837ce591a2
hyejin <yhj970102@gmail.com>
2024-01-11 18:03:47 +0900

    [Feat] color 관련 상수 변수 생성

---
+++ classum-survey/src/constants/SurveyPreviewPage/color.ts
@@ -0,0 +1,15 @@
+const COLOR_PRIMARY = "primary";
+const COLOR_SECONDARY = "secondary";
+const COLOR_ERROR = "error";
+const COLOR_WARNING = "warning";
+const COLOR_INFO = "info";
+const COLOR_SUCCESS = "success";
+
+export {
+  COLOR_PRIMARY,
+  COLOR_SECONDARY,
+  COLOR_ERROR,
+  COLOR_WARNING,
+  COLOR_INFO,
+  COLOR_SUCCESS,
+};
---
+++ classum-survey/src/constants/SurveyPreviewPage/index.ts
@@ -0,0 +1,8 @@
+export {
+  COLOR_PRIMARY,
+  COLOR_SECONDARY,
+  COLOR_ERROR,
+  COLOR_WARNING,
+  COLOR_INFO,
+  COLOR_SUCCESS,
+} from "./color";
