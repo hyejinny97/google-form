bc7f4ec73075f74c4d3b8cac870c7540951252d7
hyejin <yhj970102@gmail.com>
2024-01-11 18:04:50 +0900

    [Feat] SurveyQuestionBox에 borderColor, description, descriptionColor prop 추가

--- classum-survey/src/components/Commons/SurveyQuestionBox.tsx
+++ classum-survey/src/components/Commons/SurveyQuestionBox.tsx
@@ -1,17 +1,62 @@
 import { forwardRef } from "react";
+import { Box as MuiBox } from "@mui/material";
 import { Box } from "@components";
-import { BOX_MARGIN } from "@constants";
+import {
+  BOX_MARGIN,
+  COLOR_PRIMARY,
+  COLOR_SECONDARY,
+  COLOR_ERROR,
+  COLOR_WARNING,
+  COLOR_INFO,
+  COLOR_SUCCESS,
+} from "@constants";
+
+type ColorsType =
+  | typeof COLOR_PRIMARY
+  | typeof COLOR_SECONDARY
+  | typeof COLOR_ERROR
+  | typeof COLOR_WARNING
+  | typeof COLOR_INFO
+  | typeof COLOR_SUCCESS;
 
 interface SurveyQuestionBoxProps {
   className?: string;
-  children?: React.ReactNode | string;
+  borderColor?: ColorsType;
+  description?: React.ReactNode;
+  descriptionColor?: ColorsType;
+  children?: React.ReactNode;
 }
 
 const SurveyQuestionBox = forwardRef(
-  ({ className, children }: SurveyQuestionBoxProps, ref) => {
+  (
+    {
+      className,
+      borderColor,
+      description,
+      descriptionColor,
+      children,
+    }: SurveyQuestionBoxProps,
+    ref
+  ) => {
     return (
-      <Box className={className} sx={{ mt: BOX_MARGIN }} ref={ref}>
+      <Box
+        className={className}
+        sx={{
+          mt: BOX_MARGIN,
+          borderColor: borderColor && `${borderColor}.main`,
+        }}
+        ref={ref}
+      >
         {children}
+        <MuiBox
+          sx={{
+            mt: 1,
+            fontSize: 13,
+            color: descriptionColor && `${descriptionColor}.main`,
+          }}
+        >
+          {description}
+        </MuiBox>
       </Box>
     );
   }
