017a15763e78645b71ac7c27a7894d5f2db3e126
hyejin <yhj970102@gmail.com>
2024-01-12 17:04:27 +0900

    [Feat] SurveyPreviewPageMultipleChoiceAnswer 내 selected state 삭제 및 value, onChange prop 추가

--- classum-survey/src/components/SurveyPreviewPage/SurveyPreviewPageMultipleChoiceAnswer.tsx
+++ classum-survey/src/components/SurveyPreviewPage/SurveyPreviewPageMultipleChoiceAnswer.tsx
@@ -1,27 +1,24 @@
-import { useState } from "react";
 import { MultipleChoiceAnswer } from "@components";
 import type { OptionType } from "@stores";
 
 interface SurveyPreviewPageMultipleChoiceAnswerProps {
   options: Array<OptionType>;
+  value: number; // 선택된 questionId
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   disabled?: boolean;
 }
 
 function SurveyPreviewPageMultipleChoiceAnswer({
   options,
+  value,
+  onChange,
   disabled,
 }: SurveyPreviewPageMultipleChoiceAnswerProps) {
-  const [selected, setSelected] = useState<number>(-1);
-
-  const handleSelectChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSelected(Number(e.target.value));
-  };
-
   return (
     <MultipleChoiceAnswer
       options={options}
-      value={selected}
-      onChange={handleSelectChange}
+      value={value}
+      onChange={onChange}
       disabled={disabled}
     />
   );
