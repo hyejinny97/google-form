7071ff5f8fcc86462780ee3316a7b9435273f431
hyejin <yhj970102@gmail.com>
2024-01-14 15:02:25 +0900

    [Refactor] SurveySubmitButton 컴포넌트 내 handler 함수에서 submitSurvey dispatch 코드 제거

--- classum-survey/src/components/SurveyPreviewPage/SurveySubmitButton.tsx
+++ classum-survey/src/components/SurveyPreviewPage/SurveySubmitButton.tsx
@@ -1,15 +1,11 @@
-import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@mui/material";
-import { submitSurvey } from "@stores";
 import { PATH_SURVEY_PREVIEW_SUBMIT } from "@constants";
 
 function SurveySubmitButton() {
-  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleButtonClick = () => {
-    dispatch(submitSurvey());
     navigate(PATH_SURVEY_PREVIEW_SUBMIT);
   };
 
