46582583ca7f6aefd16a5d0036fea12e5978fcad
hyejin <yhj970102@gmail.com>
2024-01-16 18:27:20 +0900

    [Feat] sessionStorage 파일 생성 및 getSessionStorage, setSessionStorage 함수 생성

---
+++ classum-survey/src/utils/sessionStorage.ts
@@ -0,0 +1,15 @@
+const getSessionStorage = (key: string) => {
+  return (
+    sessionStorage.getItem(key) &&
+    JSON.parse(sessionStorage.getItem(key) as string)
+  );
+};
+
+const setSessionStorage = (key: string, value: any) => {
+  const stringifiedValue = JSON.stringify(value);
+  sessionStorage.setItem(key, stringifiedValue);
+
+  return sessionStorage.getItem(key);
+};
+
+export { getSessionStorage, setSessionStorage };
