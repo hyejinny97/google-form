1ae9ec354e34bff615d887ac9b90b881cd91c9df
hyejin <yhj970102@gmail.com>
2024-01-16 17:05:38 +0900

    [Feat] tryUpdateQuestionTitleSaga, tryUpdateQuestionOptionsSaga 함수 생성

--- classum-survey/src/stores/sagas/surveySaga.ts
+++ classum-survey/src/stores/sagas/surveySaga.ts
@@ -1,7 +1,18 @@
 import { debounce, put } from "redux-saga/effects";
 import type { PayloadAction } from "@reduxjs/toolkit";
-import { tryUpdateSurveyTitle, tryUpdateSurveyTitleDesc } from "../actions";
-import { updateSurveyTitle, updateSurveyTitleDesc } from "@stores";
+import {
+  tryUpdateSurveyTitle,
+  tryUpdateSurveyTitleDesc,
+  tryUpdateQuestionTitle,
+  tryUpdateQuestionOptions,
+} from "../actions";
+import type { UpdateQuestionActionPayloadType } from "../slices";
+import {
+  updateSurveyTitle,
+  updateSurveyTitleDesc,
+  updateQuestionTitle,
+  updateQuestionOptions,
+} from "@stores";
 
 function* tryUpdateSurveyTitleSaga(action: PayloadAction<string>) {
   yield put(updateSurveyTitle(action.payload));
@@ -11,7 +22,21 @@ function* tryUpdateSurveyTitleDescSaga(action: PayloadAction<string>) {
   yield put(updateSurveyTitleDesc(action.payload));
 }
 
+function* tryUpdateQuestionTitleSaga(
+  action: PayloadAction<UpdateQuestionActionPayloadType<"title">>
+) {
+  yield put(updateQuestionTitle(action.payload));
+}
+
+function* tryUpdateQuestionOptionsSaga(
+  action: PayloadAction<UpdateQuestionActionPayloadType<"options">>
+) {
+  yield put(updateQuestionOptions(action.payload));
+}
+
 export function* surveySaga() {
   yield debounce(1000, tryUpdateSurveyTitle, tryUpdateSurveyTitleSaga);
   yield debounce(1000, tryUpdateSurveyTitleDesc, tryUpdateSurveyTitleDescSaga);
+  yield debounce(1000, tryUpdateQuestionTitle, tryUpdateQuestionTitleSaga);
+  yield debounce(1000, tryUpdateQuestionOptions, tryUpdateQuestionOptionsSaga);
 }
