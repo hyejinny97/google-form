d9b5972b2b304cba02b7e2e4f50a5a850463e457
hyejin <yhj970102@gmail.com>
2024-01-16 18:29:59 +0900

    [Feat] redux store 내 survey state를 변경 시키는 action이 dispatch될 때마다 setSessionStorageSaga 함수 실행시키기

--- classum-survey/src/stores/sagas/surveySaga.ts
+++ classum-survey/src/stores/sagas/surveySaga.ts
@@ -1,4 +1,4 @@
-import { debounce, put } from "redux-saga/effects";
+import { debounce, put, call, takeEvery } from "redux-saga/effects";
 import type { PayloadAction } from "@reduxjs/toolkit";
 import {
   tryUpdateSurveyTitle,
@@ -6,32 +6,46 @@ import {
   tryUpdateQuestionTitle,
   tryUpdateQuestionOptions,
 } from "../actions";
+import { surveySlice } from "../slices";
 import type { UpdateQuestionActionPayloadType } from "../slices";
 import {
   updateSurveyTitle,
   updateSurveyTitleDesc,
+  addQuestion,
+  deleteQuestion,
+  duplicateQuestion,
   updateQuestionTitle,
+  updateQuestionType,
+  updateQuestionRequired,
   updateQuestionOptions,
+  reorderQuestions,
 } from "@stores";
+import { setSessionStorageSaga } from "./setSessionStorageSaga";
+
+const stateName = surveySlice.name;
 
 function* tryUpdateSurveyTitleSaga(action: PayloadAction<string>) {
   yield put(updateSurveyTitle(action.payload));
+  yield call(setSessionStorageSaga, stateName);
 }
 
 function* tryUpdateSurveyTitleDescSaga(action: PayloadAction<string>) {
   yield put(updateSurveyTitleDesc(action.payload));
+  yield call(setSessionStorageSaga, stateName);
 }
 
 function* tryUpdateQuestionTitleSaga(
   action: PayloadAction<UpdateQuestionActionPayloadType<"title">>
 ) {
   yield put(updateQuestionTitle(action.payload));
+  yield call(setSessionStorageSaga, stateName);
 }
 
 function* tryUpdateQuestionOptionsSaga(
   action: PayloadAction<UpdateQuestionActionPayloadType<"options">>
 ) {
   yield put(updateQuestionOptions(action.payload));
+  yield call(setSessionStorageSaga, stateName);
 }
 
 export function* surveySaga() {
@@ -39,4 +53,10 @@ export function* surveySaga() {
   yield debounce(1000, tryUpdateSurveyTitleDesc, tryUpdateSurveyTitleDescSaga);
   yield debounce(1000, tryUpdateQuestionTitle, tryUpdateQuestionTitleSaga);
   yield debounce(1000, tryUpdateQuestionOptions, tryUpdateQuestionOptionsSaga);
+  yield takeEvery(addQuestion, setSessionStorageSaga, stateName);
+  yield takeEvery(deleteQuestion, setSessionStorageSaga, stateName);
+  yield takeEvery(duplicateQuestion, setSessionStorageSaga, stateName);
+  yield takeEvery(updateQuestionType, setSessionStorageSaga, stateName);
+  yield takeEvery(updateQuestionRequired, setSessionStorageSaga, stateName);
+  yield takeEvery(reorderQuestions, setSessionStorageSaga, stateName);
 }
