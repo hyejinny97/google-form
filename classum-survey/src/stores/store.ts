5597db407a53d845504dc1b669ef5a0351d43b6c
hyejin <yhj970102@gmail.com>
2024-01-16 16:21:06 +0900

    [Feat] redux store의 middleware에 saga 연결

--- classum-survey/src/stores/store.ts
+++ classum-survey/src/stores/store.ts
@@ -1,4 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "./sagas";
 import {
   surveyReducer,
   surveySlice,
@@ -8,13 +10,19 @@ import {
   surveyPreviewAnswerSlice,
 } from "./slices";
 
+const sagaMiddleware = createSagaMiddleware();
+
 const store = configureStore({
   reducer: {
     [surveySlice.name]: surveyReducer,
     [isSurveySubmittedSlice.name]: isSurveySubmittedReducer,
     [surveyPreviewAnswerSlice.name]: surveyPreviewAnswerReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(sagaMiddleware),
 });
+
+sagaMiddleware.run(rootSaga);
 
 export { store };
 export type RootState = ReturnType<typeof store.getState>;
