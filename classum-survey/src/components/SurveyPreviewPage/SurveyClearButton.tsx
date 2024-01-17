14ed6ad08214e3ec6440afd9c9fd19edfeaa83aa
hyejin <yhj970102@gmail.com>
2024-01-17 16:23:11 +0900

    [Feat] SurveyClearButton 클릭 시 모달창이 뜨도록 구현

--- classum-survey/src/components/SurveyPreviewPage/SurveyClearButton.tsx
+++ classum-survey/src/components/SurveyPreviewPage/SurveyClearButton.tsx
@@ -1,18 +1,52 @@
+import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { Button } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+} from "@mui/material";
 import { clearSurveyPreviewAnswer } from "@stores";
 
 function SurveyClearButton() {
+  const [openModal, setOpenModal] = useState(false);
   const dispatch = useDispatch();
 
-  const handleButtonClick = () => {
+  const handleModalOpen = () => {
+    setOpenModal(true);
+  };
+
+  const handleModalClose = () => {
+    setOpenModal(false);
+  };
+
+  const handleClearButtonClick = () => {
     dispatch(clearSurveyPreviewAnswer());
+    handleModalClose();
   };
 
   return (
-    <Button color="secondary" onClick={handleButtonClick}>
-      양식 지우기
-    </Button>
+    <>
+      <Button color="secondary" onClick={handleModalOpen}>
+        양식 지우기
+      </Button>
+      <Dialog open={openModal} onClose={handleModalClose}>
+        <DialogTitle>작성한 답변을 모두 지우시겠습니까?</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            답변을 지우면 되돌릴 수 없습니다.
+            <br />
+            정말 지우시겠습니까?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClearButtonClick}>네</Button>
+          <Button onClick={handleModalClose}>아니요</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
 
