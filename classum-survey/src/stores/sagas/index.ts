249f161fe888941e49982cb7cb58275ac10fc487
hyejin <yhj970102@gmail.com>
2024-01-17 14:57:52 +0900

    [Feat] surveyPreviewAnswerSaga 함수 생성

--- classum-survey/src/stores/sagas/index.ts
+++ classum-survey/src/stores/sagas/index.ts
@@ -1,6 +1,7 @@
 import { all } from "redux-saga/effects";
 import { surveySaga } from "./surveySaga";
+import { surveyPreviewAnswerSaga } from "./surveyPreviewAnswerSaga";
 
 export function* rootSaga() {
-  yield all([surveySaga()]);
+  yield all([surveySaga(), surveyPreviewAnswerSaga()]);
 }
