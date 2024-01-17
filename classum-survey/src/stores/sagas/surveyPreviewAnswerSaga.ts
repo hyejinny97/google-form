4eb94d924a79578aa7d3e91aa6e67db26c41a115
hyejin <yhj970102@gmail.com>
2024-01-17 15:53:52 +0900

    [Feat] surveyPreviewAnswerSaga에서 clearSurveyPreviewAnswer action 호출 시 deleteSessionStorageSaga 함수를 실행하는 코드 추가

--- classum-survey/src/stores/sagas/surveyPreviewAnswerSaga.ts
+++ classum-survey/src/stores/sagas/surveyPreviewAnswerSaga.ts
@@ -1,12 +1,18 @@
 import { takeEvery } from "redux-saga/effects";
 import { setSessionStorageSaga } from "./setSessionStorageSaga";
+import { deleteSessionStorageSaga } from "./deleteSessionStorageSaga";
 import { surveyPreviewAnswerSlice } from "../slices";
-import { updateSurveyPreviewAnswer } from "@stores";
+import { updateSurveyPreviewAnswer, clearSurveyPreviewAnswer } from "@stores";
 
 const stateName = surveyPreviewAnswerSlice.name;
 
 function* surveyPreviewAnswerSaga() {
   yield takeEvery(updateSurveyPreviewAnswer, setSessionStorageSaga, stateName);
+  yield takeEvery(
+    clearSurveyPreviewAnswer,
+    deleteSessionStorageSaga,
+    stateName
+  );
 }
 
 export { surveyPreviewAnswerSaga };
